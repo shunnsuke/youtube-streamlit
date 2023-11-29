1c7f8e87eb8a3afbd22593b6be1cf3441bb23f2b
tsutsumi <sat112ds1_two_room@yahoo.co.jp>
2023-11-29 19:38:49 +0900

    1st commit

---
+++ Desktop/python_app/main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import time
+st.title('Streamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!!'
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}') 
+    bar.progress(i + 1)
+    time.sleep(0.1) 
+    
+'Done!!!'                        
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+    
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く') 
+#text = st.text_input('あなたが好きな趣味を教えてください',)
+#'あなたの趣味は',text, 'です。'
+
+#condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+#'コンディション：',condition
+
+#if st.checkbox('show Image'):
+#    img = Image.open('absolute.jpg')
+ #   st.image(img, caption='absolute', use_column_width=True)
+
